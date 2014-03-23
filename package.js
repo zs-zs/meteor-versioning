b84c7eef370b3b9e018224e27e241e7bdd038e73
zs-zs <zsolt.zsigmondi@hotmail.com>
2014-03-23 20:29:50 +0100

    Change indentation from spaces to tabs

--- package.js
+++ package.js
@@ -1,12 +1,12 @@
 Package.describe({
-  summary: "Undo/redo, improved conflict resolution and transactions for Meteor collections."
+	summary: "Undo/redo, improved conflict resolution and transactions for Meteor collections."
 });
 
 Package.on_use(function (api, where) {
-  where = where || ['client', 'server'];
-  api.use(['underscore', 'logger', 'i18n', 'mongo-livedata', 'livedata', 'random'], where);
-  api.add_files('crdt.js', where);
-  api.add_files('versioned-collection.js', where);
-  api.add_files('transactions.js', where);
-  api.add_files('messages.js', where);
+	where = where || ['client', 'server'];
+	api.use(['underscore', 'logger', 'i18n', 'mongo-livedata', 'livedata', 'random'], where);
+	api.add_files('crdt.js', where);
+	api.add_files('versioned-collection.js', where);
+	api.add_files('transactions.js', where);
+	api.add_files('messages.js', where);
 });
